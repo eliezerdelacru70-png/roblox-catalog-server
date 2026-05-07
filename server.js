e2efa5d760a41fa378c39697b3112f4baff67135
eliezerdelacru70-png <eliezerdelacru70@gmail.com>
2026-05-07 11:27:13 -0400

    Clarify search category comment in server.js
    Updated comment to clarify search category description.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 8080;
 app.get('/api/catalog/search', async (req, res) => {
     try {
         const { keyword = "shirt", cursor = "" } = req.query;
-        // Buscamos en categoría 0 para que salga de todo (3D y Clásico)
+        // Buscamos en categoría 0 para mezclar todo (3D y clásico)
         const url = `https://catalog.roproxy.com/v1/search/items/details?category=0&limit=30&keyword=${keyword}&cursor=${cursor}`;
 
         const response = await fetch(url);
